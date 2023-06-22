a20b4ea80b1062f57592fdc9ec7f653f7ec4ed8a
Arnav7107 <arnavverma345@gmail.com>
2023-06-23 00:47:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = new express();
+const path = require('path');
+app.use(express.static(__dirname + '/'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.send('About');
+});
+
+app.get('/createnew', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'createnew.html'));
+});
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(".post-wrapper").slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     nextArrow:$('.next'),
+//     prevArrow:$('.prev'),
+//   });
