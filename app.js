b6b5ea065722a61b453a0bd75774a5448d86da26
Arnav Verma <109273538+Arnav7107@users.noreply.github.com>
2023-07-09 23:34:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ app.use(express.static(__dirname + '/'));
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
-
 app.get('/about', (req, res) => {
   res.send('About');
 });
